fd7ce5aa01faec4f29e60bd8e00834c45a26fd4c
Zhecho Zhechev <zhechev.zhecho@gmail.com>
2016-01-17 23:50:07 +0200

    remove some spaces

--- python-notes.py
+++ python-notes.py
@@ -330,83 +330,97 @@ print("-------------------------Задача 6: Matchmaking---------------------
 # 			Копирайте и използвайте в кода си тези данни:
 # 
 people = [
-              {
-                       'name': "Мария",
-                       'interests': ['пътуване', 'танци', 'плуване', 'кино'],
-                       'gender': "female",
-                   },
-              {
-                       'name': "Диана",
-                       'interests': ['мода', 'спортна стрелба', 'четене',
-                                     'скандинавска поезия'],
-                       'gender': "female",
-                   },
-              {
-                       'name': "Дарина",
-                       'interests': ['танци', 'покер', 'история', 'софтуер'],
-                       'gender': "female",
-                   },
-              {
-                       'name': "Лилия",
-                       'interests': ['покер', 'автомобили', 'танци', 'кино'],
-                       'gender': "female",
-                   },
-              {
-                       'name': "Галя",
-                       'interests': ['пътуване', 'автомобили', 'плуване',
-                                     'баскетбол'],
-                       'gender': "female",
-                   },
-              {
-                       'name': "Валерия",
-                       'interests': ['плуване', 'покер', 'наука', 'скандинавска поезия'],
-                       'gender': "female",
-                                         },
-                   {
-                        'name': "Ина",
-                        'interests': ['кино', 'лов със соколи', 'пътуване', 'мода'],
-                        'gender': "female",
-                        },
-                   {
-                        'name': "Кирил",
-                        'interests': ['баскетбол', 'автомобили', 'кино', 'наука'],
-                        'gender': "male",
-                        },
-                   {
-                        'name': "Георги",
-                        'interests': ['автомобили', 'футбол', 'плуване', 'танци'],
-                        'gender': "male",
-                        },
-                   {
-                            'name': "Андрей",
-                            'interests': ['футбол', 'скандинавска поезия', 'история', 'танци'],
-                            'gender': "male",
-                        },
-                   {
-                            'name': "Емил",
-                            'interests': ['летене', 'баскетбол', 'софтуер', 'наука'],
-                            'gender': "male",
-                        },
-                   {
-                            'name': "Димитър",
-                            'interests': ['футбол', 'лов със соколи', 'автомобили', 'баскетбол'],
-                            'gender': "male",
-                        },
-                   {
-                            'name': "Петър",
-                            'interests': ['пътуване', 'покер', 'баскетбол', 'лов със соколи'],
-                            'gender': "male",
-                        },
-                   {
-                            'name': "Калоян",
-                            'interests': ['история', 'покер', 'пътуване', 'автомобили'],
-                            'gender': "male",
-                        },
+     {
+         'name': "Мария",
+         'interests': ['пътуване', 'танци', 'плуване', 'кино'],
+         'gender': "female",
+     },
+     {
+         'name': "Диана",
+         'interests': ['мода', 'спортна стрелба', 'четене', 'скандинавска поезия'],
+         'gender': "female",
+     },
+     {
+         'name': "Дарина",
+         'interests': ['танци', 'покер', 'история', 'софтуер'],
+         'gender': "female",
+     },
+     {
+         'name': "Лилия",
+         'interests': ['покер', 'автомобили', 'танци', 'кино'],
+         'gender': "female",
+     },
+     {
+         'name': "Галя",
+         'interests': ['пътуване', 'автомобили', 'плуване',
+                       'баскетбол'],
+         'gender': "female",
+     },
+     {
+         'name': "Валерия",
+         'interests': ['плуване', 'покер', 'наука', 'скандинавска поезия'],
+         'gender': "female",
+     },
+     {
+          'name': "Ина",
+          'interests': ['кино', 'лов със соколи', 'пътуване', 'мода'],
+          'gender': "female",
+     },
+     {
+          'name': "Кирил",
+          'interests': ['баскетбол', 'автомобили', 'кино', 'наука'],
+          'gender': "male",
+     },
+     {
+          'name': "Георги",
+          'interests': ['автомобили', 'футбол', 'плуване', 'танци'],
+          'gender': "male",
+     },
+     {
+         'name': "Андрей",
+         'interests': ['футбол', 'скандинавска поезия', 'история', 'танци'],
+         'gender': "male",
+     },
+     {
+         'name': "Емил",
+         'interests': ['летене', 'баскетбол', 'софтуер', 'наука'],
+         'gender': "male",
+     },
+     {
+         'name': "Димитър",
+         'interests': ['футбол', 'лов със соколи', 'автомобили', 'баскетбол'],
+         'gender': "male",
+     },
+     {
+         'name': "Петър",
+         'interests': ['пътуване', 'покер', 'баскетбол', 'лов със соколи'],
+         'gender': "male",
+     },
+     {
+         'name': "Калоян",
+         'interests': ['история', 'покер', 'пътуване', 'автомобили'],
+         'gender': "male",
+     },
 ]
 # make set from interests & foreach item from that set get male & female that
 # have such interest
+boys = []
+girls = []
 for person in people:
-    print (person.get('grender', None)) person.get
+    if person.get('grender',None) == 'male':
+        boys.append(person)
+    else:
+        girls.append(person)
+
+for boy in boys:
+    for girl in girls:
+        boy_interests = set(boy['interests'])
+        girl_interests = set(girl['interests'])
+        if boy_interests & girl_interests:
+            print("{} и {} - интереси: {}".format(boy['name'], girl['name'],
+                  boy_interests & girl_interests ))
+            break
+
 
 
 
